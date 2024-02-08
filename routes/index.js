ca4567d8e5f4654169fc0cdbcb15a3c274f8ea0c
Pacific-Feng <pacfeng@gmail.com>
2024-02-08 22:56:54 +0800

    First version: still practicing

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@
 var express = require('express');
 var router = express.Router();      // create a "router" instance
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* get home page. */
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Todo page */
+// 建立 todos data
+const todos = ['first todo', 'second todo', 'third todo']
+
+router.get('/todos', (req, res) => {
+  res.render('todos', { todos })  // 尋找名為 todos 的視圖模板文件
+})
 
 module.exports = router;
